0d42dc8efea5f4ed10de65b570022e4c6985f47a
Tejus S <tejuss@Mac.lan>
2025-06-28 07:08:56 +0530

    Remove old trademarks

--- frontend/app/(presentation-generator)/theme/page.tsx
+++ frontend/app/(presentation-generator)/theme/page.tsx
@@ -3,8 +3,8 @@ import ThemePage from './ThemePage'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: "Theme Selection | Presenton.ai",
-    description: "Select a Presenton theme which will be suitable for your presentation",
+    title: "Theme Selection",
+    description: "Select a theme which will be suitable for your presentation",
 }
 
 const page = () => {
